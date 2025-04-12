1d472daa6b5cb1e3721b5017df6aa21a5e230d03
Yanis Cherfa <private@email.com>
2025-04-12 02:15:19 +0200

    Toast bug fix

--- src/Views/Form/Layout/Toast.php
+++ src/Views/Form/Layout/Toast.php
@@ -6,5 +6,34 @@ use RocketPhp\RocketUI\Views\Form\Layout\Abstract\AbstractLayout;
 
 class Toast extends AbstractLayout
 {
+    private string $type;
+    private string $description;
+
+    public function __construct(\DOMElement $toastNode)
+    {
+        parent::__construct($toastNode);
+        $this->type = $toastNode->getAttribute("type") ?? 'default';
+        $this->description = $toastNode->getAttribute("description") ?? '';
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function omitJson(mixed $data): array
+    {
+        $jsonResponse = [
+            'type' => $this->getType(),
+            'description' => $this->getDescription(),
+        ];
+
+        return $jsonResponse;
+    }
 
 }
